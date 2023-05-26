d4b1848a9b8ac7cbe75031e6e7cd4ffa648372ca
aleksandrakirpenko <82666714+sarcasmqueen1@users.noreply.github.com>
2023-05-26 10:43:01 +0200

    Risk score changes

--- medex/controller/risk_score.py
+++ medex/controller/risk_score.py
@@ -4,22 +4,15 @@ from medex.controller.helpers import get_prediction_service
 
 risk_controller = Blueprint('risk_controller', __name__)
 
-# example data since we don't know yet how to access the actual database
-data = [
-    {
-        'Disease': 'Diabetes',
-        'Score': '20%',
-        'Patient': 'John Doe',
-        'Birthdate': '08/05/2023'
-    }
-]
-
 
 @risk_controller.route('/', methods=['GET'])
-def risk_score():
-    print("Hello")
-    print(get_prediction_service().get_risk_score_for_name_id('5f2b9323c39ee3c861a7b382d205c3d3'))
-    print(get_prediction_service().get_risk_score_for_name_id('5890595e16cbebb8866e1842e4bd6ec7', disease="CHD"))
+def get_html():
+    return render_template('risk_score.html')
 
-    return render_template('risk_score.html', data=data)
+# @risk_controller.route('/', methods=['GET'])
+# def risk_score():
+#   print("Hello")
+#  print(get_prediction_service().get_risk_score_for_name_id('5f2b9323c39ee3c861a7b382d205c3d3'))
+# print(get_prediction_service().get_risk_score_for_name_id('5890595e16cbebb8866e1842e4bd6ec7', disease="CHD"))
 
+# return render_template('risk_score.html', data=data)
