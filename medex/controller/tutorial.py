ba0bfc81d6dc99436132b17d7ee7ec9efbade878
SIlvia Heuberger <silviaheuberger6@gmail.com>
2023-06-29 12:56:03 +0200

    removed everything except add_pre and models

--- medex/controller/tutorial.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-tutorial_controller = Blueprint('tutorial_controller', __name__)
-
-
-@tutorial_controller.route('/', methods=['GET'])
-def tutorial():
-    return render_template('tutorial.html')
--- medex/dto/measurement.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-from pydantic import BaseModel
-
-
-class MeasurementInfo(BaseModel):
-    display_name: str
-    values: List[str]
